585f62803e74ab1e19360eb30e5f2f85487792b2
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2021-10-06 17:11:09 +0900

    linked Database

---
+++ src/main/java/com/board/dao/UserAuthDAO.java
@@ -0,0 +1,17 @@
+package com.board.dao;
+
+import com.board.domain.CustomUserDetails;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("userAuthDAO")
+public class UserAuthDAO {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    public CustomUserDetails getUserById(String username) {
+        return sqlSession.selectOne("user.selectUserById", username);
+    }
+}
--- src/main/java/com/board/domain/SecurityVO.java
+++
@@ -1,4 +0,0 @@
-package com.board.domain;
-
-public class SecurityVO {
-}
